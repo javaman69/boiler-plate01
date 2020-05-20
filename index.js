253cbe6751f272f2925a3722b5e5acc4f265f20e
javaman69 <javamania69@gmail.com>
2020-05-20 11:25:19 +0900

    비밀정보보호

--- index.js
+++ index.js
@@ -1,13 +1,39 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const bodyParser = require("body-parser");
+
+const config = require("./config/key");
+
+const { User } = require("./models/User");
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://kimpd1210:java4198!!@cluster0-sat8i.mongodb.net/test?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log("MongoDB Connected..."))
   .catch(err => console.log(err))
 
+
 app.get('/', (req, res) => res.send("Hello World!"))
+
+
+app.post('/register', (req, res) => {
+  // 회원가입에 필요한 정보 mongo DB 에 저장
+
+  const user = User(req.body)
+
+
+  user.save((err, userInfo) => {
+    if(err) return res.json({ success: false, err })
+    return res.status(200).json({
+      success: true
+    })
+  })
+})
+
 
 app.listen(port, () => console.log(`Example app listen at http://localhost:${port}`))
